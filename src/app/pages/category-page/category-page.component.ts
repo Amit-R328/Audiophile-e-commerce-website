feb674248d7820db57305506aad06c1b0a664c3f
Neptucom-ST <amit@neptucom.com>
2026-06-13 04:10:09 +0300

    feat: update category page to use async pipe for category and enhance product fetching logic

--- src/app/pages/category-page/category-page.component.ts
+++ src/app/pages/category-page/category-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Product } from '../../models/product.model';
 import { ProductService } from '../../services/product.service';
 
@@ -11,7 +11,7 @@ import { ProductService } from '../../services/product.service';
   styleUrls: ['./category-page.component.scss']
 })
 export class CategoryPageComponent implements OnInit {
-  category = '';
+  category$!: Observable<string>;
   products$!: Observable<Product[]>;
 
   constructor(
@@ -20,11 +20,15 @@ export class CategoryPageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.products$ = this.route.paramMap.pipe(
-      switchMap((params: any) => {
-        this.category = params.get('category') ?? '';
-        return this.productService.getByCategory(this.category);
-      })
+    const category$ = this.route.paramMap.pipe(
+      map(params => params.get('category') ?? '')
+    );
+
+    this.category$ = category$;
+    this.products$ = category$.pipe(
+      switchMap(category => this.productService.getByCategory(category)),
+      // Newest first (highest id), matching the design order.
+      map(products => [...products].sort((a, b) => b.id - a.id))
     );
   }
 }
